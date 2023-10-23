bb5982fd163073d696cf3ee6f3a48da9acd014aa
Mark Hollomon <mhhollomon@gmail.com>
2023-10-23 14:49:53 -0400

    Add copyright message to main source files

--- Source/Starp.hpp
+++ Source/Starp.hpp
@@ -1,3 +1,16 @@
+/****
+ * Starp - Stable Random Arpeggiator Plugin 
+ * Copyright (C) 2023 Mark Hollomon
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the 
+ * Free Software Foundation, either version 3 of the License, or (at your 
+ * option) any later version. This program is distributed in the hope that it 
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the LICENSE file
+ * in the root directory.
+ ****/
+
+
 #pragma once
 
 #if !defined(STARP_DEBUG)
